6ab13eee194871bb1b469bf4a718b623293fd98e
uzzal71 <uzzalroy.acm@gmail.com>
2022-05-31 11:19:28 +0600

    initial commit project settings

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+dotenv.config();
+
+// body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// serving static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () =>
+    console.log(`your server is running on port ${PORT}`)
+);
